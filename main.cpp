c2d2d1d258776192b1d569708703ef885367ea3c
Jeremy Evans <jeremy@chiefgeek.io>
2023-01-23 10:57:04 -0500

    Learned GLClearColor and other rendering commands.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
 int main()
 {
@@ -37,6 +38,13 @@ int main()
 
 	while (!glfwWindowShouldClose(window))
 	{
+		// process input
+		processInput(window);
+
+		// All render commands go here.
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
@@ -49,4 +57,10 @@ int main()
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
 }
