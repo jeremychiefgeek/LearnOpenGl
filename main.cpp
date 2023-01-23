24494aaf597e0ad7edb423b04e85b6bb84cfdbe1
Jeremy Evans <jeremy@chiefgeek.io>
2023-01-23 10:42:20 -0500

    Dull window has been completed.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,51 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
 int main()
 {
+	// Initialize and instantiate GLFW window
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create winddow and set it to the context on current thread.
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGl", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Initialize Glad before calling any OpenGl functions because this manages correct functions.
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Faild to initialize GLAD" << std::endl;
+		return -1;
+	}
+	
+	// Set Callback for resizeable window
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Let OpenGl know the size of the window;
+	glViewport(0, 0, 800, 600);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
 }
